8075de68729fbc49f2bd61007605b2003bee7279
Rick Boss <rickeboss@gmail.com>
2022-12-09 20:52:11 -0800

    part 1

--- src/day9.rs
+++ src/day9.rs
@@ -8,31 +8,37 @@ pub fn run() -> (String, String) {
     return (run_part1(inp.clone()), run_part2(inp.clone()));
 }
 
-enum Move {
-    Up(u32),
-    Down(u32),
-    Right(u32),
-    Left(u32),
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+#[derive(Debug)]
+struct Move {
+    direction: Direction,
+    spaces: u32,
 }
 
 fn parse_input(inp: String) -> Vec<Move> {
-    let mut moves = vec![];
-    for line in inp.lines() {
+    inp.lines().map(|line| {
         let parts: Vec<&str> = line.split(" ").collect();
         let spaces = parts[1].parse().unwrap();
-        moves.push(match parts[0] {
-            "U" => Move::Up(spaces),
-            "D" => Move::Down(spaces),
-            "R" => Move::Right(spaces),
-            "L" => Move::Left(spaces),
+        let direction = match parts[0] {
+            "U" => Direction::Up,
+            "D" => Direction::Down,
+            "R" => Direction::Right,
+            "L" => Direction::Left,
             _ => unreachable!("unhandled move"),
-        })
-    }
+        };
 
-    moves
+        Move{direction, spaces}
+    }).collect::<Vec<Move>>()
 }
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
 struct Point {
     x: i32,
     y: i32,
@@ -58,12 +64,10 @@ fn print_rope(head: Point, tail: Point, start: Point, size: (i32, i32)) {
     }
 }
 
-fn print_visited(visited: HashSet<Point>) {
+fn print_visited(visited: &HashSet<Point>) {
     let max = visited
         .iter()
         .fold((0, 0), |acc, Point { x, y }| (acc.0.max(*x), acc.1.max(*y)));
-
-    dbg!(max);
 
     for y in 0..max.1 + 1 {
         let line = (0..max.0 + 1)
@@ -82,35 +86,77 @@ fn print_visited(visited: HashSet<Point>) {
     }
 }
 
+fn get_tail_move(tail: Point, head: Point) -> Point {
+    if tail.x.max(head.x) - tail.x.min(head.x) < 2 && tail.y.max(head.y) - tail.y.min(head.y) < 2 {
+        return tail;
+    }
+
+    if tail.x == head.x {
+        // left - right
+        Point {
+            x: tail.x,
+            y: tail.y + (if head.y > tail.y { 1 } else { -1 }),
+        }
+    } else if tail.y == head.y {
+        // up - down
+        Point {
+            x: tail.x + (if head.x > tail.x { 1 } else { -1 }),
+            y: tail.y,
+        }
+    } else {
+        let x = if tail.x > head.x {
+            tail.x - 1
+        } else {
+            tail.x + 1
+        };
+        let y = if tail.y > head.y {
+            tail.y - 1
+        } else {
+            tail.y + 1
+        };
+        // diagonal
+        Point { x, y }
+    }
+}
+
+fn do_moves(mv: Move, head: &mut Point, tail: &mut Point, visited: &mut HashSet<Point>) {
+    for _ in 0..mv.spaces {
+        match mv.direction {
+            Direction::Down => {
+                head.y -= 1;
+            },
+            Direction::Up => {
+                head.y += 1;
+            },
+            Direction::Left => {
+                head.x -= 1;
+            },
+            Direction::Right => {
+                head.x += 1;
+            },
+        };
+
+        let new_pos = get_tail_move(tail.clone(), head.clone());
+        tail.x = new_pos.x;
+        tail.y = new_pos.y;
+        visited.insert(new_pos);
+    }
+}
+
 fn run_part1(inp: String) -> String {
     let moves = parse_input(inp);
-    // let visited: HashSet<Point> = HashSet::new();
+    let mut visited: HashSet<Point> = HashSet::new();
 
-    let visited: HashSet<Point> = HashSet::from([
-        Point { x: 2, y: 0 },
-        Point { x: 3, y: 0 },
-        Point { x: 3, y: 1 },
-        Point { x: 4, y: 1 },
-        Point { x: 1, y: 2 },
-        Point { x: 2, y: 2 },
-        Point { x: 3, y: 2 },
-        Point { x: 4, y: 2 },
-        Point { x: 4, y: 3 },
-        Point { x: 0, y: 4 },
-        Point { x: 1, y: 4 },
-        Point { x: 2, y: 4 },
-        Point { x: 3, y: 4 },
-    ]);
+    let mut head = Point { x: 0, y: 0 };
+    let mut tail = Point { x: 0, y: 0 };
 
-    print_visited(visited);
-    print_rope(
-        Point { x: 3, y: 2 },
-        Point { x: 4, y: 1 },
-        Point { x: 0, y: 4 },
-        (6, 5),
-    );
+    for mv in moves {
+        do_moves(mv, &mut head, &mut tail, &mut visited)
+    }
 
-    "".to_string()
+    print_visited(&visited);
+
+    visited.len().to_string()
 }
 
 fn run_part2(inp: String) -> String {
@@ -131,12 +177,49 @@ mod tests {
     #[test]
     fn test_part1() {
         let res = run_part1(INPUT.to_string());
+        assert_eq!(res, "13")
+    }
+
+    #[test]
+    #[ignore]
+    fn test_part2() {
+        let res = run_part2(INPUT.to_string());
         assert_eq!(res, "1")
     }
 
     #[test]
-    fn test_part2() {
-        let res = run_part2(INPUT.to_string());
-        assert_eq!(res, "1")
+    fn test_get_tail_move() {
+        // move right
+        assert_eq!(
+            get_tail_move(Point { x: 1, y: 1 }, Point { x: 3, y: 1 }),
+            Point { x: 2, y: 1 }
+        );
+        // move left
+        assert_eq!(
+            get_tail_move(Point { x: 3, y: 1 }, Point { x: 1, y: 1 }),
+            Point { x: 2, y: 1 }
+        );
+        // move "down"
+        assert_eq!(
+            get_tail_move(Point { x: 1, y: 1 }, Point { x: 1, y: 3 }),
+            Point { x: 1, y: 2 }
+        );
+        // move "up"
+        assert_eq!(
+            get_tail_move(Point { x: 1, y: 3 }, Point { x: 1, y: 1 }),
+            Point { x: 1, y: 2 }
+        );
+
+        // diag up & right
+        assert_eq!(
+            get_tail_move(Point { x: 1, y: 3 }, Point { x: 2, y: 1 }),
+            Point { x: 2, y: 2 },
+        );
+
+        // diag up & right still
+        assert_eq!(
+            get_tail_move(Point { x: 1, y: 3 }, Point { x: 3, y: 2 }),
+            Point { x: 2, y: 2 },
+        );
     }
 }
